e96ecbbd89d9710993dcc71f50f2fdbdf53f15f5
Utkarsh Patel <imutkarshpatel@gmail.com>
2020-01-21 12:43:30 +0530

    Create sol.cpp

---
+++ .lab/sol.cpp
@@ -0,0 +1,13 @@
+/**
+ * Name        :
+ * Creator     : Utkarsh Patel, 18EC30048 (utkarshpatel@iitkgp.ac.in)
+ * Date        : Jan 21, 2020
+ * Description :
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    return 0;
+}
