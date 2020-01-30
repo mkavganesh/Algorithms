05accde4db601a973180185e357e517b96b6d1b2
Utkarsh Patel <imutkarshpatel@gmail.com>
2020-01-30 22:46:08 +0530

    Removed bug
    This algorithm should always print unit fraction where den > num, previous routine failed if a > b. Fixed this issue :)

--- greedy/egyptianFraction.cpp
+++ greedy/egyptianFraction.cpp
@@ -8,25 +8,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void egyptfrac (double a, double b) {
+unordered_map<int, bool> upmap;
+
+void egyptfrac (double a, double b, int q) {
     if (a == 1.0) {
         cout << int(a) << "/" << int(b) << endl;
         return;
     }
     int r = ceil(b / a);
+    if (r == 1) r++;
+    if (r == q) r++;
     int num = int(a) * r - int (b);
     int den = int(b) * r;
     int g = __gcd(num, den);
     num /= g;
     den /= g;
     cout << 1 << "/" << r << " + ";
-    egyptfrac(double(num), double(den));
+    egyptfrac(double(num), double(den), r);
 }
 
 int main () {
     double a, b;
     cout << "Enter a b: ";
     cin >> a >> b;
-    egyptfrac(a, b);
+    egyptfrac(a, b, 1);
     return 0;
 }
