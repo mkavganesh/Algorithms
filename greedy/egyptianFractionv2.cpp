c36aef54af58fdab49b18b5fef67414f669e6945
Utkarsh Patel <imutkarshpatel@gmail.com>
2020-02-04 01:25:33 +0530

    Create egyptianFractionv2.cpp

---
+++ greedy/egyptianFractionv2.cpp
@@ -0,0 +1,34 @@
+/**
+ * @author utkarshpatel (https://github.com/utkarshiitkgp)
+ * 
+ * Egyptian Fraction Problem
+ * Type: Greedy algorithm
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<int, bool> upmap;
+
+void egyptfrac (double a, double b) {
+    if (a == 1.0) {
+        cout << int(a) << "/" << int(b) << endl;
+        return;
+    }
+    int r = ceil(b / a);
+    int num = int(a) * r - int (b);
+    int den = int(b) * r;
+    int g = __gcd(num, den);
+    num /= g;
+    den /= g;
+    cout << 1 << "/" << r << " + ";
+    egyptfrac(double(num), double(den), r);
+}
+
+int main () {
+    double a, b;
+    cout << "Enter a b: ";
+    cin >> a >> b;
+    egyptfrac(a, b);
+    return 0;
+}
